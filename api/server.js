5dccfb1add974cc272db233b8af7ee951d3c0301
richard-mcghee <richardjmcghee@yahoo.com>
2020-09-10 19:22:57 -0400

    got things working on this rather confusingly worded project after 1:1 help with my TL, Loralie

--- api/server.js
+++ api/server.js
@@ -6,6 +6,49 @@ const server = express();
 
 server.use(express.json());
 
-// write endpoints in here 
+server.get("/", (req, res) => {
+    db("accounts")
+    .then(account => {
+        res.status(200).json({ data: account })
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
+}) 
+
+server.post("/", (req, res) => {
+    db("accounts").insert(req.body)
+    .then(newAcc => {
+        res.status(200).json({ data: newAcc })
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
+})
+
+server.put("/:id", (req, res) => {
+    const { id } = req.params
+    const newUpdate = req.body
+
+    db("accounts").update(newUpdate).where({ id })
+    .then(account => {
+        res.status(200).json({ data: account })
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
+})
+
+server.delete("/:id", (req, res) => {
+    const id = req.params.id
+
+    db("accounts").where({ id }).del()
+    .then(numDel => {
+        res.status(200).json({ data: numDel })
+    })
+    .catch(err => {
+        res.status(500).json({ error: err })
+    })
+})
 
 module.exports = server;
